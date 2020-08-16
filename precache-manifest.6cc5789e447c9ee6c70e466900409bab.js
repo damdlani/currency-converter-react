124572463677b864e57c77e3a6a57b1235f1a4e8
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-16 21:31:35 +0200

    Updates

---
+++ precache-manifest.6cc5789e447c9ee6c70e466900409bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602f86b701f865faf574f0a703a0091d",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "970ef428801679cba204",
+    "url": "/currency-converter-react/static/css/main.d60f3cd0.chunk.css"
+  },
+  {
+    "revision": "905154bdb5ec382c7432",
+    "url": "/currency-converter-react/static/js/2.ca30315d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.ca30315d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970ef428801679cba204",
+    "url": "/currency-converter-react/static/js/main.d3000957.chunk.js"
+  },
+  {
+    "revision": "015aa95f07033766b799",
+    "url": "/currency-converter-react/static/js/runtime-main.9bac7e2b.js"
+  }
+]);
