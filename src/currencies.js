fcd196511de59f91c3e495a988ee1da06fd887b1
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-18 08:03:27 +0200

    currencies array created as single  element

---
+++ src/currencies.js
@@ -0,0 +1,26 @@
+export const currencies = [
+    {
+      id: 1, 
+      name: "EUR",
+      fullname: "Euro",
+      value: 4.3988,
+    },
+    {
+      id: 2, 
+      name: "USD",
+      fullname: "Dolar amerykański",
+      value: 3.7227,
+    },
+    {
+      id: 3, 
+      name: "GBP",
+      fullname: "Funt szterling",
+      value: 4.8692,
+    },
+    {
+      id: 4, 
+      name: "CHF",
+      fullname: "Frank szwajcarski",
+      value: 4.0906,
+    },
+  ];
