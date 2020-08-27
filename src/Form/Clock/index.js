f540236cb83a03b9a10eeb9007f64fd0d03e9c82
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-27 11:50:32 +0200

    useEffect properly written

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
+
+
 import './style.css';
 
 const Clock = () => {
 
     const [date, setDate] = useState(new Date());
 
-    const intervalID = setInterval(() => {
-        setDate(new Date())
-    }, 1000);
+    useEffect(() => {      
+        const intervalID = setInterval(() => {
+            setDate(date => date = new Date())
+        }, 1000);
 
-    useEffect(() => {
-        clearInterval(intervalID)
+        return () => { 
+            clearInterval(intervalID)
+        };
     }, [])
 
     const day = date.toLocaleDateString(undefined, {
@@ -21,9 +25,12 @@ const Clock = () => {
     const time = date.toLocaleTimeString();
 
     return (
+        <>
         <div className="clock">
             Dzisiaj jest {day}, {time}
         </div>
+        </>
+
     );
 };
 
