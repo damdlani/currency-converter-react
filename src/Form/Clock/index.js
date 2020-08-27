1a80fd6bab0fae4d79dd18db302b0a24b4abb9a1
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-27 11:16:08 +0200

    clock code simplified

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,39 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const Clock = () => {
-    let newDate = new Date();
-    const [date, setDate] = useState(newDate.toLocaleDateString(undefined, {day: "numeric", month: "long",}));
-    const [time, setTime] = useState(newDate.toLocaleTimeString());
-    const [day, setDay] = useState(newDate.toLocaleDateString(undefined, {weekday: "long"}))
 
-    const updateDate = () => {
-        setDate(newDate.toLocaleDateString(
-            undefined, {
-                day: "numeric", month: "long",
-            }
-        ));
-    }
-    const updateTime = () => {
-        setTime(newDate.toLocaleTimeString());
-    }
-    const updateDay = () => {
-        setDay(newDate.toLocaleDateString(
-            undefined, {
-                weekday: "long"
-            }
-        ));
-    }
-    setInterval(() => {
-        newDate = new Date();
-        updateDate();
-        updateTime();
-        updateDay();    
-    }, 1000)
+    const [date, setDate] = useState(new Date());
+
+    const intervalID = setInterval(() => {
+        setDate(new Date())
+    }, 1000);
+
+    useEffect(() => {
+        clearInterval(intervalID)
+    }, [])
+
+    const day = date.toLocaleDateString(undefined, {
+                    day: "numeric", 
+                    month: "long",
+                    weekday: "long"
+                });
+    const time = date.toLocaleTimeString();
 
     return (
         <div className="clock">
-            Dzisiaj jest {day},  {date}, {time}
+            Dzisiaj jest {day}, {time}
         </div>
     );
 };
