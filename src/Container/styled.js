8949fdbbea35174d5e46a809a276553b381d5ef0
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-09-02 08:45:25 +0200

    StyledComponent added

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,13 +1,14 @@
-.container {
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
     padding: 20px 100px;
     min-height: 100vh;
     position: relative;
     width: 100%;
     max-width: 1000px;
     margin: 0 auto;
-}
-@media (max-width: 500px) {
-    .container {
+
+    @media (max-width: 500px) {
         padding: 10px;
     }
-}
+`;
