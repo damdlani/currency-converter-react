6f4f55e1eeea21d047675d0d9f397b3ca6aca91d
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-09-18 08:01:37 +0200

    only for school purpose, to check what's wrong

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from './Container';
 import Footer from './Footer';
 import Form from './Form';
@@ -7,6 +7,23 @@ import { currencies } from './currencies'
 function App() {
   const [result, setResult] = useState()  
 
+  useEffect(() => {
+    //https://api.exchangeratesapi.io/latest?base=PLN gdy daje ten link, to ok, ale jak robię w nim literówkę,
+    //to nie pokazuje wgl statusu błędu, przez co nie działa wyjątek !response.ok i nie mogę dać swojego błędu
+      fetch("data.json")
+          .then(response => {
+              if(!response.ok) {
+                throw new Error();
+              }
+              
+              return response;
+          })
+          .then(response => response.json())
+          .then(response => console.log(response))
+          .catch(error => console.error(error));
+    }, [])   
+   
+  
   const countResult = (amount, currency) => {
     const rate = currencies.find(({shortname}) => shortname === currency).value;
     
@@ -16,7 +33,9 @@ function App() {
       currency,
     })
   };
+
   
+
   return (
     <Container>
         <Form 
