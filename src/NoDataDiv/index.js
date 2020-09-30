94c5e7fee1c81d2f0a83676fd1802e11ca3619c9
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-09-30 09:08:35 +0200

    more enjoyable error and loading handling added

--- src/NoDataDiv/index.js
+++ src/NoDataDiv/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Div } from './styled'
+import loading from '../images/loading.gif'
+import error from '../images/error.png'
 
 export const NoDataDiv = ({statusInfo}) => {
     return <Div>
-        {statusInfo}
+        {statusInfo === "error" ? 
+        <div><img src={error} width={"200px"} alt=""/><br/>Niestety wystąpił błąd.<br/><br/> Proszę sprawdzić połączenie z internetem i ponownie załadować stronę.</div> : 
+        <div><img src={loading} alt=""/><br/>Trwa ładowanie danych, proszę czekać</div>}
     </Div>
 };
--- src/NoDataDiv/styled.js
+++ src/NoDataDiv/styled.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 export const Div = styled.div`
     width: 100%;
-    padding: 20px 30px;
-    background-color: rgb(255, 255, 255, .9);
+    padding: 20px 30px 40px;
+    background-color: rgb(255, 255, 255);
     border: hsl(0, 0%, 30%) solid 1px;
     border-radius: 30px;
     text-align: center;
@@ -12,5 +12,6 @@ export const Div = styled.div`
 
     @media (max-width: 767px) {
         padding: 20px;
+        font-size: 12px;
     }
 `;
